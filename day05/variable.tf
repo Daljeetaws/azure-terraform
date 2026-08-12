d62a2b3ed41a2c6e53bb3a967d52ab5ecf88fc74
unknown <daljeetaws@gmail.com>
2026-08-12 18:19:34 +0530

    Initial commit

---
+++ day05/variable.tf
@@ -0,0 +1,43 @@
+variable "environment" {
+    type = string
+    description = "the env type"
+    default = "prod"
+}
+variable "allowed_locations" {
+    type = list(string)
+    description = "list of allowed locations"
+    default = [ "West Europe", "North Europe" , "East US" ]
+  
+}
+variable "location" {
+  default = "West Europe"
+  type = string
+  
+}
+variable "resource_tags" {
+    type = map(string)
+    description = "tags to apply to the resources"
+    default = {
+      "environment" = "staging"
+      "managed_by" = "terraform"
+      "department" = "devops"
+    }
+  
+}
+# Tuple type
+variable "network_config" {
+  type        = tuple([string, string, number])
+  description = "Network configuration (VNET address, subnet address, subnet mask)"
+  default     = ["10.0.0.0/16", "10.0.2.0", 24]
+}
+variable "allowed_vm_sizes" {
+  type        = list(string)
+  description = "Allowed VM sizes"
+  default     = ["Standard_DS1_v2", "Standard_DS2_v2", "Standard_DS3_v2"]
+}
+variable "storage_account_name" {
+  type = set(string)
+  default = [ "techtutorials11", "techtutorials12" ]
+
+  
+}
